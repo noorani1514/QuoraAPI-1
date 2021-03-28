9b1f9ca9c222f269f06def25730e27a4acdeefca
Noor <noorani1514@gmail.com>
2021-03-28 18:36:26 +0530

    Created createAnswer and editAnswerContent

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -0,0 +1,53 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.AnswerEditRequest;
+import com.upgrad.quora.api.model.AnswerEditResponse;
+import com.upgrad.quora.api.model.AnswerRequest;
+import com.upgrad.quora.api.model.AnswerResponse;
+import com.upgrad.quora.service.business.AnswerBusinessService;
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.exception.AnswerNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
+
+@RestController
+@RequestMapping("/")
+public class AnswerController {
+
+    @Autowired
+    private AnswerBusinessService answerBusinessService;
+
+    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerResponse> createAnswer(
+            @RequestHeader("authorization") final String accessToken,
+            @PathVariable("questionId") final String questionId,
+            AnswerRequest answerRequest)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        AnswerEntity answerEntity = new AnswerEntity();
+        answerEntity.setAns(answerRequest.getAnswer());
+        answerEntity = answerBusinessService.createAnswer(answerEntity, accessToken, questionId);
+        AnswerResponse answerResponse = new AnswerResponse();
+        answerResponse.setId(answerEntity.getUuid());
+        answerResponse.setStatus("ANSWER CREATED");
+        return new ResponseEntity<>(answerResponse, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/answer/edit/{answerId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerEditResponse> editAnswer(
+            @RequestHeader("authorization") final String accessToken,
+            @PathVariable("answerId") final String answerId,
+            AnswerEditRequest answerEditRequest)
+            throws AuthorizationFailedException, AnswerNotFoundException {
+        AnswerEditResponse answerEditResponse = new AnswerEditResponse();
+        AnswerEntity answerEntity = new AnswerEntity();
+        answerEntity.setAns(answerEditRequest.getContent());
+        answerEditResponse.setId(answerEntity.getUuid());
+        answerEditResponse.setStatus("ANSWER EDITED");
+        return new ResponseEntity<AnswerEditResponse>(answerEditResponse, HttpStatus.OK);
+    }
+}
---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
@@ -0,0 +1,78 @@
+package com.upgrad.quora.service.business;//package com.upgrad.quora.service.business;
+
+
+import com.upgrad.quora.service.dao.AnswerDao;
+import com.upgrad.quora.service.dao.QuestionDao;
+
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AnswerNotFoundException;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.InvalidQuestionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class AnswerBusinessService {
+
+    @Autowired
+    private AnswerDao answerDao;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity createAnswer(AnswerEntity answerEntity, final String authorization, final String questionId)
+            throws AuthorizationFailedException, InvalidQuestionException {
+        UserAuthEntity userAuthEntity = authenticationService.checkAuthentication(authorization);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to post an answer");
+        }
+        QuestionEntity questionEntity = questionDao.getQuestionById(questionId);
+        if (questionEntity == null) {
+            throw new InvalidQuestionException("QUES-001", "The question entered is invalid");
+        }
+        answerEntity.setUuid(UUID.randomUUID().toString());
+        answerEntity.setDate(LocalDateTime.now());
+        answerEntity.setQuestion(questionEntity);
+        answerEntity.setUser(userAuthEntity);
+        return answerDao.createAnswer(answerEntity);
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity editAnswerContent(
+            final String authorization, final String answerId, final String newAnswer)
+            throws AnswerNotFoundException, AuthorizationFailedException {
+        UserAuthEntity userAuthEntity = authenticationService.checkAuthentication(authorization);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to edit an answer");
+        }
+        AnswerEntity answerEntity = answerDao.getAnswerById(answerId);
+        if (answerEntity == null) {
+            throw new AnswerNotFoundException("ANS-001", "Entered answer uuid does not exist");
+        }
+        if (!answerEntity.getUser().getUuid().equals(userAuthEntity.getUser().getUuid())) {
+            throw new AuthorizationFailedException(
+                    "ATHR-003", "Only the answer owner can edit the answer");
+        }
+        answerEntity.setAns(newAnswer);
+        answerDao.editAnswerContent(answerEntity);
+        return answerEntity;
+    }
+}
